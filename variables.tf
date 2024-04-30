111975f0fa6a6effd675b6dd384f64fb2a0e5cad
Andriy.Zelenyy <azelyony@gmail.com>
2024-04-30 23:10:54 +0300

    Add flux

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "GCP project name"
+}
+
+variable "GOOGLE_REGION" {
+  type        = string
+  default     = "us-central1-c"
+  description = "GCP region to use"
+}
+
+variable "GOOGLE_BUCKET" {
+  type        = string
+  description = "GCS bucket name"
+}
+
+variable "GKE_NUM_NODES" {
+  type        = number
+  default     = 2
+  description = "GKE nodes number"
+}
+
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "GitHub owner repository to use"
+}
+
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GitHub personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitOps repository"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = "Flux manifests subdirectory"
+}
